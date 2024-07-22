0f81bc907693d2b683be770f64380db88a5aa6d2
Bren016 <153107940+Bren016@users.noreply.github.com>
2024-07-22 15:54:54 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = document.querySelector('.navbar__menu')
+
+var tabLinks = document.getElementsByClassName("tab-links");
+var tabContents = document.getElementsByClassName("tab-contents");
+var tabImages = {
+    'skills'    : 'images/coaching2.jpg',
+    'experience': 'images/laurentide.jpg',
+    'education' : 'images/hikeOakcrop.jpg'
+}
+
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
+
+
+function opentab(tabname) {
+    for(tabLink of tabLinks){
+        tabLink.classList.remove("active-link");
+
+    }
+    for(tabContent of tabContents){
+        tabContent.classList.remove("active-tab");
+
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+
+    document.getElementById('about__img').src = tabImages[tabname]
+}
